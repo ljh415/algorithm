ba671df83d06561f7f1b46a254be2ce35d62f965
ljh415 <ljh415@gmail.com>
2020-08-03 11:20:06 +0900

    20_08_02 (전화번호 목록 review)

--- p/p_전화번호 목록.py
+++ p/p_전화번호 목록.py
@@ -1,16 +1,18 @@
-def solution(phone_book) :
-    answer = True
-    phone_book.sort()
+def solution(phone_book):
+
+    phone_book = sorted(phone_book)
     print(phone_book)
-    for i in range(len(phone_book)) :
-        for j in range(i+1, len(phone_book)-1) :
-            if str(j) in str(i) :
+
+    for i in range(len(phone_book)-1):
+        for j in range(i+1, len(phone_book)):
+            if phone_book[i] in phone_book[j]:
                 return False
+
     return True
 
-if __name__ == "__main__" :
-    # phone_book =    ['119', '97674223', '1195524421']
-    # phone_book = ['123', '456', '789']
-    phone_book = ['12', '123', '1235', '567', '88']
+if __name__ == '__main__':
+    phone_book = ['119', '97674223', '1195524421']
+    # phone_book = ['123','456','789']
+    # phone_book = ['12','123','1235','567','88']
 
     print(solution(phone_book))
