ee1dd2254ae83a7e46ced913c7817aceb7214377
ljh415 <ljh415@gmail.com>
2020-08-24 03:04:15 +0900

    프린터 코드수정

--- p/p_프린터.py
+++ p/p_프린터.py
@@ -24,3 +24,10 @@ def solution(priorities, location):
             Max_in_lst = max(priorities)
 
     return answer
+
+if __name__ == '__main__':
+
+    # priorities, location = [2, 1, 3, 2], 2
+    priorities, location = [1, 1, 9, 1, 1, 1], 0
+
+    print(solution(priorities, location))
