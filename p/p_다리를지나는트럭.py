733cbfc0caa9ad09e0c08ff3a847991952f628d1
ljh415 <ljh415@gmail.com>
2020-08-24 03:37:38 +0900

    테스트 케이스 5번 시간초과

--- p/p_다리를지나는트럭.py
+++ p/p_다리를지나는트럭.py
@@ -3,13 +3,14 @@ def solution(bridge_length, weight, truck_weights):
     # 트럭저장하는 큐 (리스트로 만듬)
     t_list = []
 
-    # 트럭뽑는 함수
+    # 트럭을 한 대씩 뽑는다
     for i in truck_weights :
         q_truck = i
         while q_truck != 0 : # 트럭이 빠질 때까지 확인하기 위해서
             if len(t_list) == bridge_length : # t_list의 길이가 bridge_length와 같아지면 트럭빼기
                 t_list.pop() # 시간을 더해줘야 할 것 같은데, 밑에 if, else 구문에서 시간을 넣어주면 된다.
 
+            # sum()부분에서 시간초과
             if (sum(t_list)+q_truck) <= weight :     # t_list의 합이 weight 이하이면 트럭을 계속 넣을 수 있다.
                 t_list.insert(0,q_truck)
                 q_truck = 0
@@ -24,8 +25,19 @@ def solution(bridge_length, weight, truck_weights):
 
     return answer
 
+# def solution(bridge_length, weight, truck_weights) :
+#     answer = []
+#
+#     while truck_weights :
+#         answer
+#
+#     return answer
+
 if __name__ == '__main__':
 
-    bridge_length, weight, truck_weight = 2, 10, [7, 4, 5, 6]
+    bridge_length, weight, truck_weights = 2, 10, [7, 4, 5, 6]
+    # bridge_length, weight, truck_weights = 100, 100, [10]
+    # bridge_length, weight, truck_weights = 100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
 
-    print(solution(bridge_length, weight, truck_weight))
+
+    print(solution(bridge_length, weight, truck_weights))
