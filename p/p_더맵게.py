8cb6651c21dc3cbe13de44986c4b7921cd0ee356
jh-ubuntu <ljh415@gmail.com>
2020-08-30 18:34:11 +0900

    20.08.30(더 맵게, heap)

---
+++ p/p_더맵게.py
@@ -0,0 +1,30 @@
+import heapq
+
+
+def solution(scoville, K):
+    answer = 0
+    a = 0
+    count = 0
+    heap = []
+    # 힙생성
+    for i in scoville:
+        heapq.heappush(heap, i)
+    # 최소힙구성, 가장 작은값을 먼저 a에 저장
+    # 그 다음 작은 값에 2를 곱하여 다시 힙에 추가
+    while heap[0] < K:
+        try:
+            a = heapq.heappop(heap)
+            a += 2 * heapq.heappop(heap)
+            heapq.heappush(heap, a)
+        except:
+            return -1
+
+        count += 1
+
+    answer = count
+
+    return answer
+
+if __name__ == '__main__':
+    scoville, K = [1, 2, 3, 9, 10, 12], 7
+    print(solution(scoville, K))
